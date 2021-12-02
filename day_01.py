ed97f587932d4bd0681ef937a233159c42888a70
conjonorama <connor.r.brown@outlook.com>
2021-12-01 20:00:51 -0800

    Day_01

---
+++ day_01.py
@@ -0,0 +1,30 @@
+# Day 1 - Part 1
+
+# Reading in the file
+with open('inputs/01_01.txt') as f:
+    puzzle_input = [int(line.strip()) for line in f]
+
+def part_one(inputs):
+    count = 0
+
+    previous = inputs[0]
+    for i in range(1, len(inputs)):
+        current = inputs[i]
+        if current > previous:
+            count += 1
+        previous = current
+    
+    return count
+
+# part_one(puzzle_input)
+
+def part_two(inputs):
+    new_list = []
+    for i in range (0, len(inputs)-2):
+        addition = inputs[i] + inputs[i+1] + inputs[i+2]
+        new_list.append(addition)
+    
+    return part_one(new_list)
+
+
+part_two(puzzle_input)
